dfbb865af6bda1c9a0ae2e6b8338c634e5474004
david_albert <davidalbert6938@gmail.com>
2025-04-10 14:53:47 +0200

    interrupt branch initial commit

---
+++ test_interrupt.py
@@ -0,0 +1,21 @@
+##################################################
+# FarmerSoft Open Interface Test
+##################################################
+# FarmerSoft © 2024
+# By Daweed
+##################################################
+
+# Standard Modules Import
+import logging
+import time
+
+# SimOpInt Module Import
+from SimOpInt.SimOpIntLogger import SimOpIntLogger
+from SimOpInt.SimOpInt import SimOpInt
+
+simopintlogger = SimOpIntLogger('SimOpInt', 'Logs', 'simopint.log')
+logger = simopintlogger.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Test Interface Initialisation
+INT1 = SimOpInt('Config/Interfaces/SimOpIntTest', 'SimOpIntTest.json')
